938014b9a7c2a992548e3a2646a956589591227f
Slawomir Czerwinski <slawomir@czerwinski.it>
2021-01-12 00:15:31 +0000

    Remove docs v1.0.0-ALPHA1

--- v1.0.0-ALPHA1/glimpse/core/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":glimpse:core:dokkaHtml/androidMain": [], ":glimpse:core:dokkaHtml/commonMain": [], ":glimpse:core:dokkaHtml/desktopMain": [":glimpse:core:dokkaHtml/commonMain"]}'
--- v1.0.0-ALPHA1/glimpse/ui/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":glimpse:ui:dokkaHtml/androidMain": [], ":glimpse:ui:dokkaHtml/commonMain": [], ":glimpse:ui:dokkaHtml/desktopMain": [":glimpse:ui:dokkaHtml/commonMain"]}'
