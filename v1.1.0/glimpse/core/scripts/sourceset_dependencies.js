074b3e9ef3edf0ce10e3249934ad1fb18fe59d15
GitHub Action <action@github.com>
2021-02-27 16:16:52 +0000

    Update docs v1.1.0

---
+++ v1.1.0/glimpse/core/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":glimpse:core:dokkaHtml/androidMain": [":glimpse:core:dokkaHtml/commonMain"], ":glimpse:core:dokkaHtml/commonMain": [], ":glimpse:core:dokkaHtml/desktopMain": [":glimpse:core:dokkaHtml/commonMain"]}'
---
+++ v1.1.0/glimpse/obj/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":glimpse:obj:dokkaHtml/androidMain": [":glimpse:obj:dokkaHtml/commonMain"], ":glimpse:obj:dokkaHtml/commonMain": [], ":glimpse:obj:dokkaHtml/desktopMain": [":glimpse:obj:dokkaHtml/commonMain"]}'
